db8076d02e770235bb46508a80e98be44d1d833f
goodperiodt <ilypsj@gmail.com>
2025-05-27 09:54:19 +0900

    util 클래스 추가

---
+++ src/algo2025/may/Util.java
@@ -0,0 +1,14 @@
+package algo2025.may;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Util {
+    public static Scanner kb;
+    public static Logger log;
+
+    static {
+        kb = new Scanner(System.in);
+        log = Logger.getLogger("log");
+    }
+}

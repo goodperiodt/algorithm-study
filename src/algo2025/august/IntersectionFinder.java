7e0eb787837810c1487beea133c1e476f4f42225
goodperiodt <ilypsj@gmail.com>
2025-08-15 00:08:33 +0900

    refactoring

--- src/algo2025/august/IntersectionFinder.java
+++ src/algo2025/august/IntersectionFinder.java
@@ -4,45 +4,40 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionFinder {
-    public int[] getIntersection(int[] nums1, int[] nums2) {
-        /* Set<Integer> set = new HashSet<>();
+    public int[] getIntersection(int[] nums1, int[] nums2) { // findIntersection() 메서드명도 괜찮은 듯
+        Set<Integer> set1 = toSet(nums1);
+        Set<Integer> set2 = toSet(nums2);
+        Set<Integer> commonElements = findCommonElements(set1, set2);
+        return convertSetToArray(commonElements);
+    }
 
-        for (int j : nums1) {
-            for (int k : nums2) {
-                if (j == k) {
-                    set.add(j);
-                }
-            }
+    // 배열을 전달받고, Set 자료 구조를 사용하여 배열의 중복된 요소를 제거하기에
+    // 처음에 removeDuplicates 를 작명했는데, 반환 타입이 set 이고,
+    // Set 을 이용한다는 것 자체가 요소의 중복이 없다는 것을 의미하니까
+    // toSet() 으로 메서드명을 작명해도 될 것 같다.
+    private Set<Integer> toSet(int[] nums) {
+        Set<Integer> toUniqueSet = new HashSet<>();
+        for(int num:nums) {
+            toUniqueSet.add(num);
         }
-        // Set 타입의 컬렉션을 배열로 반환하기.
-        // return set.stream().mapToInt(n->n.intValue()).toArray();
-        return set.stream().mapToInt(Integer::intValue).toArray();
-    */
-    // 두 배열중 하나를 Set 에 저장하고, 나머지 배열을 순회하며 존재 여부를 체크한다.
-        Set<Integer> set1 = new HashSet<>(); // nums1 의 배열에 저장된 요소들 중복없이 담기 위한 Set 타입 자료구조
-        Set<Integer> result = new HashSet<>(); // set 과 nums2 에 담긴 공통 요소를 담기위한 자료구조
+        return toUniqueSet;
+    }
 
-        for(int i: nums1)
-            set1.add(i);
+    private Set<Integer> findCommonElements(Set<Integer> set1, Set<Integer> set2) {
+        if(set1.size()>set2.size())
+            return findCommonElements(set2, set1);
 
-        // set 에는 중복되지 않은 nums1 의 요소들이 들어있는 상태
-        /* for(int i: set1) {
-            for(int j:nums2) {
-                if(i==j) result.add(i);
-            }
-        } */
+        Set<Integer> commonElementsSet = new HashSet<>();
+        for(int num:set1)
+            if(set2.contains(num)) commonElementsSet.add(num);
+        return commonElementsSet;
+    }
 
-        for(int num: nums2) {
-            if(set1.contains(num))
-                result.add(num);
-        }
-
-        int[] intersections = new int[result.size()];
+    private int[] convertSetToArray(Set<Integer> commonElements) {
+        int[] intersections = new int[commonElements.size()];
         int i=0;
-        for(int num:result) {
-            intersections[i]=num; // intersections[i++]=num;
-            i++;
-        }
+        for(int num:commonElements)
+            intersections[i++]=num;
         return intersections;
     }
 }
